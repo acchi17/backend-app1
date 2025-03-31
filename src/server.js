0a69833e633bfcf9a87b6df5d3403f7f6af2592a
acchi17 <grf317@gmail.com>
2025-03-31 22:03:04 +0900

    Port setting changed.

--- src/server.js
+++ src/server.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`サーバーが${PORT}番ポートで起動しました`);
 });
